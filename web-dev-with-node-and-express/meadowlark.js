6ed76899eda17896c086bd9ae49a390c2423fd60
weeshin <weeshin@foxmail.com>
2022-06-01 22:11:45 +0800

    add: codes for chapter8 of web-dev-with-node-and-express

--- web-dev-with-node-and-express/meadowlark.js
+++ web-dev-with-node-and-express/meadowlark.js
@@ -1,45 +1,78 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const multiparty = require('multiparty');
+
+const weatherMiddlware = require('./lib/middleware/weather');
+const handlers = require('./lib/handlers');
 
 const app = express();
 
-const hbs = handlebars.create({ defaultLayout: 'main' });
+// configure Handlebars view engine
+const hbs = handlebars.create({
+  defaultLayout: 'main',
+  helpers: {
+    section: function (name, options) {
+      if (!this._sections) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
+    },
+  },
+});
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.set('views', './views');
+app.disable('x-powered-by');
 
-app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.render('home'));
+app.use(express.static(__dirname + '/public'));
 
-const fortunes = [
-  'Conquer your fears or they will conquer you.',
-  'Rivers need springs.',
-  "Do not fear what you don't know.",
-  'You will have a pleasant surprise.',
-  'Whenever possible, keep it simple.',
-];
+app.use(weatherMiddlware);
 
-app.get('/about', (req, res) => {
-  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+app.get('/', handlers.home);
 
-  res.render('about', { fortune: randomFortune });
+// handlers for browser-based form submission
+app.get('/newsletter-signup', handlers.newsletterSignup);
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess);
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou);
+
+// handlers for fetch/JSON form submission
+app.get('/newsletter', handlers.newsletter);
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup);
+
+// vacation photo contest
+app.get('/contest/vacation-photo', handlers.vacationPhotoContest);
+app.get('/contest/vacation-photo-ajax', handlers.vacationPhotoContestAjax);
+app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+  const form = new multiparty.Form();
+  form.parse(req, (err, fields, files) => {
+    if (err)
+      return handlers.vacationPhotoContestProcessError(req, res, err.message);
+    console.log('got fields: ', fields);
+    console.log('and files: ', files);
+    handlers.vacationPhotoContestProcess(req, res, fields, files);
+  });
+});
+app.get(
+  '/contest/vacation-photo-thank-you',
+  handlers.vacationPhotoContestProcessThankYou
+);
+app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
+  const form = new multiparty.Form();
+  form.parse(req, (err, fields, files) => {
+    if (err)
+      return handlers.api.vacationPhotoContestError(req, res, err.message);
+    handlers.api.vacationPhotoContest(req, res, fields, files);
+  });
 });
 
-// custom 404 page
-app.use((req, res) => {
-  res.status(404);
-  res.render('404');
-});
-
-// custom 500 page
-app.use((err, req, res, next) => {
-  console.error(err.message);
-  res.status(500);
-  res.render('500');
-});
+app.use(handlers.notFound);
+app.use(handlers.serverError);
 
 if (require.main === module) {
   app.listen(port, () => {
